9be986b428221e13cfca2b7c764b5d197227e6d5
xujichang <xujichang@outlook.com>
2018-11-02 11:51:52 +0800

    1. 调整对系统序列号获取的方式

--- xutil/src/main/java/me/xujichang/util/system/SystemInfo.java
+++ xutil/src/main/java/me/xujichang/util/system/SystemInfo.java
@@ -1,6 +1,17 @@
 package me.xujichang.util.system;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+
+import com.google.common.base.Strings;
+
 import java.lang.reflect.Method;
+
+import me.xujichang.util.thirdparty.DeviceUtils;
+import me.xujichang.util.tool.LogTool;
 
 /**
  * Des:
@@ -29,5 +40,51 @@ public class SystemInfo {
             ret = "";
         }
         return ret;
+    }
+
+    public static String getSn(Context context) {
+        //常规
+        //ro.serialno
+        //ro.boot.serialno
+        //特有
+        //gsm.serial
+        String[] propertys = {"gsm.serial", "ro.boot.serialno", "ro.serialno",};
+
+        String sn = null;
+        for (int i = 0; i < propertys.length; i++) {
+            sn = getAndroidOsSystemProperties(propertys[i]);
+            if (!Strings.isNullOrEmpty(sn)) {
+                break;
+            }
+        }
+        if (Strings.isNullOrEmpty(sn)) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    sn = getOldSn();
+                } else {
+                    sn = Build.getSerial();
+                }
+            } else {
+                sn = Build.SERIAL;
+            }
+        }
+        if (Strings.isNullOrEmpty(sn)) {
+            sn = getOldSn();
+        }
+        LogTool.d("sn:" + sn);
+        return sn;
+    }
+
+    public static String getOldSn() {
+        LogTool.d("oldSn:" + DeviceUtils.getAndroidID());
+        return DeviceUtils.getAndroidID();
     }
 }
